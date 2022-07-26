54b68d9241141571de6d5ad266d8cbec255b62a9
Furqan <mfurqanhashmiis@gmail.com>
2022-07-26 00:32:11 -0400

    Completed READ operation. /item/list route will noe display all items in list

--- inventory_tracker/inventory_register/views.py
+++ inventory_tracker/inventory_register/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import ItemForm
+from .models import InventoryItem
 
 # Create your views here.
 
 def item_list(request):
-    return render(request, "inventory_register/item_list.html")
+    context = {'item_list':InventoryItem.objects.all()}
+
+    return render(request, "inventory_register/item_list.html",context)
 
 # GET/POST requests for inset/update operations
 def item_form(request):
@@ -12,11 +15,11 @@ def item_form(request):
         form = ItemForm()
         return render(request, "inventory_register/item_form.html", {'form':form})
     else:
-        print("****************////////////// POST REQUEST //////////////****************")
         form = ItemForm(request.POST)
         if form.is_valid():
-            print("////////////// FORM IS VALID //////////////")
             form.save()
+        else:
+            print("Form Entries were invalid")
         return redirect('/item/list')
 
 # Delete operation
