ef1e6b97b19684280d0c70501cdee092bc91de10
Furqan <mfurqanhashmiis@gmail.com>
2022-07-25 21:41:20 -0400

    Set up templates using bootstrap

--- inventory_tracker/inventory_register/views.py
+++ inventory_tracker/inventory_register/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def item_list(request):
-    
-    return
+    return render(request, "inventory_register/item_list.html")
 
 # GET/POST requests for inset/update operations
 def item_form(request):
-    
-    return
+    return render(request, "inventory_register/item_form.html")
 
 # Delete operation
 def item_delete(request):
-    
     return
